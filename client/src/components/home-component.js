504f8fc66a5359181486d33c0900cb64bad4f052
Julie0000 <119852424+Julie0000@users.noreply.github.com>
2023-05-30 12:45:31 +0800

    add register link

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const HomeComponent = () => {
   return (
@@ -26,13 +26,13 @@ const HomeComponent = () => {
               <p>
                 學生可以註冊他們喜歡的課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
               </p>
-              <button
+              <Button
                 className="btn btn-outline-light"
                 type="button"
-                to="/register"
+                herf="/register"
               >
-                <Link> 登錄會員、或者註冊一個帳號</Link>
-              </button>
+                登錄會員、或者註冊一個帳號
+              </Button>
             </div>
           </div>
           <div className="col-md-6">
@@ -41,13 +41,13 @@ const HomeComponent = () => {
               <p>
                 您可以通過註冊成為一名講師，並開始製作在線課程。本網站僅供練習之用，請勿提供任何個人資料，例如信用卡號碼。
               </p>
-              <button
+              <Button
                 className="btn btn-outline-secondary"
                 type="button"
-                to="/register"
+                herf="/register"
               >
-                <Link>今天開始開設課程</Link>
-              </button>
+                今天開始開設課程
+              </Button>
             </div>
           </div>
         </div>
